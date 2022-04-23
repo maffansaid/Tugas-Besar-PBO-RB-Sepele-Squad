b3d55581f74984557f045cd4e049bc8ad881c41f
MRozinAsySyaddad <103322478+MRozinAsySyaddad@users.noreply.github.com>
2022-04-23 16:33:18 +0700

    Update main.py
    membenarkan indentasi

--- Petualangan Si Pele/main.py
+++ Petualangan Si Pele/main.py
@@ -11,11 +11,11 @@ pygame.display.set_caption('Petualangan Si Pele By Sepele.SQD')
 berjalan = True
 while berjalan:
 	Layar.fill((255, 255, 255))
-  
-  for acara in pygame.event.get():
-    if acara.type == pygame.QUIT:
-      berjalan = False
 
-  pygame.display.flip()
+	for acara in pygame.event.get():
+		if acara.type == pygame.QUIT:
+			berjalan = False
+
+	pygame.display.flip()
 
 pygame.quit()
