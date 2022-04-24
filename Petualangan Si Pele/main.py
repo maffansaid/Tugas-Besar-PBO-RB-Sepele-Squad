3aac1ac2078ca7c3e449da3c309883d2514556b8
maffansaid <103346388+maffansaid@users.noreply.github.com>
2022-04-24 14:19:01 +0700

    membuat class global dan class tombol

--- Petualangan Si Pele/main.py
+++ Petualangan Si Pele/main.py
@@ -3,7 +3,24 @@ import pygame, pygame.locals
 UKURAN_LAYAR = (850, 500)
 
 pygame.init()
+class GLOBAL(pygame.sprite.Sprite):
+	def __init__(self, **variabel):
+		super(GLOBAL, self).__init__()
+		self.id = variabel['id']
+		self.ukuran = (variabel['panjang'], variabel['lebar'])
+		self.pos = (variabel['x'], variabel['y'])
+		self.file = variabel['gambar']
+		self.gambar = pygame.transform.scale(self.file, self.ukuran)
+		self.posisi = self.gambar.get_rect(center = self.pos)
+	def aktif(self):
+		pass
+	def aksi(self):
+		pass
 
+class Tombol(GLOBAL):
+	def __init__(self, **variabel):
+		pass
+	
 Layar = pygame.display.set_mode(UKURAN_LAYAR)
 
 pygame.display.set_caption('Petualangan Si Pele By Sepele.SQD')
